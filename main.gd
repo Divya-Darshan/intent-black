38911ba6fb8f959e473e14f76ed04eca5fbf3e66
Divya Darshan <parukrish.darshan20@gmail.com>
2026-08-18 20:25:40 +0530

    p2

--- main.gd
+++ main.gd
@@ -1,22 +1,11 @@
 extends Node2D
 
 func _ready() -> void:
-	# Only execute intent logic if running natively on Android
-	if OS.get_name() == "Android":
-		_launch_dialer()
+	# Launch default dialer
+	OS.shell_open("tel:")
 	
-	# Terminate the Godot app immediately so it clears from memory
+	# Wait 100ms for Android to process intent before quitting
+	await get_tree().create_timer(0.1).timeout
+	
+	# Close app immediately
 	get_tree().quit()
-
-func _launch_dialer() -> void:
-	# Access Android's Intent class
-	var intent_class = JavaClassWrapper.wrap("android.content.Intent")
-	var action_dial = intent_class.ACTION_DIAL
-	
-	# Create new Intent(ACTION_DIAL)
-	var intent = intent_class.new(action_dial)
-	
-	# Fetch the active Godot Android Activity and launch the dialer
-	var godot_activity = Engine.get_singleton("AndroidRuntime")
-	var current_activity = godot_activity.get_activity()
-	current_activity.startActivity(intent)
